cca747d0d527adb07ca78b7089b5c238ec66f942
Daniel Au <dakmau@gmail.com>
2017-08-09 15:56:32 -0700

    Working users count

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 var express = require('express');
+var app = express();
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-var app = express();
+let usersCount = 0;
+
+// Socket logic
+io.on('connect', function(socket){
+  usersCount++;
+  console.log(`Users count: ${usersCount}`);
+
+  socket.on('disconnect', function(socket){
+    usersCount--;
+    console.log(`Users count: ${usersCount}`);
+  });
+
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +36,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', function(req, res, next){
+  req.usersCount = usersCount;
+  next();
+}, index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -40,4 +59,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+module.exports = {app, server};
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(req.usersCount);
+  res.render('index', { usersCount: req.usersCount });
 });
 
 module.exports = router;
